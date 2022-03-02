3bf203304ea0f3abf3cbbbfddf7f91795745368e
Nicolas <nicolaslibert.pro@gmail.com>
2022-03-02 14:55:47 +0100

    Chap. 7 complété

--- 07-classes/04-getter-setter/script.js
+++ 07-classes/04-getter-setter/script.js
@@ -5,10 +5,28 @@
     // your code here
     class Person
     {
-        
+        constructor(lastname, firstname)
+        {
+            this.lastname = lastname;
+            this.firstname = firstname;
+        }
+
+        get name()
+        {
+            return `${this.lastname} ${this.firstname}`;
+        }
+
+        set name(name)
+        {
+            name = this.name();
+            let nameDivide = name.split(' ');
+            return nameDivide;
+        }
     }
     
     document.getElementById("run").addEventListener("click", () => {
-
+        
+        let newInstance = new Person("Fierarh", "Myreina");
+        console.log(newInstance.name);
     });
 })();
